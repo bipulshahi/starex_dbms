f62a37bbaafd35dd319c97eb7cb7bf391b927993
Bipul Shahi <bipulshahi25@gmail.com>
2025-03-04 15:12:27 +0530

    Add files via upload

--- starex_sql_1/sql_functions_2.sql
+++ starex_sql_1/sql_functions_2.sql
@@ -1,3 +1,5 @@
+show databases;
+
 use starexdb;
 
 create table employees (emp_id int primary key, first_name varchar(50),
@@ -31,19 +33,22 @@ select substr(first_name , 1,3) from employees;
 
 select concat(substr(first_name , 1,3), substr(join_date,1,4)) as password from employees;
 
+select * from employees;
 #Calculate yearly salary of each employee
+select salary , salary*12 as yearly_salary from employees;
 
 #Round up and round down the salary of employees
+select salary, ceil(salary) as round_up , floor(salary) as round_down from employees;
 
 #Round up the salary to 1 decimal place
+select salary, round(salary,1) as rounded_salary from employees;
 
 #Minimum and Maximum salaries among the employees
+select min(salary) , max(salary) from employees;
+select least(23,21,7,25,29) as least_value;
 
 #Convert salary value to character
+select cast(salary as char) from employees;
 
 #Find sample where salary data is missing
-
-
-
-
-
+select * from employees where salary is null;
