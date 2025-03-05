018ed1a02eaad8edc0e5bb31a9364a8cd34c249b
Bipul Shahi <bipulshahi25@gmail.com>
2025-03-05 14:26:36 +0530

    Add files via upload

--- starex_sql_2/sql_subqueries.sql
+++ starex_sql_2/sql_subqueries.sql
@@ -1,5 +1,6 @@
 #Subqueries - exist, in, all ,any
-
+show databases;
+use starexdb;
 show tables;
 
 create table students(student_id int primary key,
@@ -48,39 +49,56 @@ select * from students where score > all(select score from students where class 
 select * from students where score > any(select score from students where class = 11);
 
 #count total number of students
+select count(*) as total_count from students;
 
 #find highest and lowest scores
+select max(score) as max_score, min(score) as min_score from students;
 
 #find average of all scores
+select avg(score) as avg_score from students;
 
 #find total of all scores
+select sum(score) as avg_score from students;
 
 #find number of students in each class
+select class,count(*) from students group by class;
 
 #find length of each student name
+select name,length(name) from students;
 
 #replace 'a' with 'A' in student names
+select name, replace(name , 'a' , 'A') from students;
 
 #Concatenate name and class
+select concat(name , ' ' , class) from students;
 
 #find student who joined in year 2022
+select * from students where year(join_date)=2022;
 
 #find student who joined in year august 2022
+select * from students where year(join_date) = 2022 and month(join_date) = 8;
 
 #find student who joined on 15th day of any month
+select * from students where day(join_date) = 15;
 
 #Find how many days each student has been in school
+select curdate();
+select datediff(curdate() , join_date) as number_of_days from students;
 
 #Find student who score greater then 80
+select * from students where score > 80;
 
 #Find student who score lesser then 70
+select * from students where score < 70;
 
 #Find student who scored 60 or more
+select * from students where score >= 60;
 
 #Find student who score between then 50 and 90
+select * from students where score between 50 and 90;
 
 #Find student who joined between then 2021-01-01 and 2023-01-01
-
+select * from students where join_date between '2021-01-01' and '2023-01-01';
 
 
 
