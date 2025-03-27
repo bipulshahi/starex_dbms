b1e0264a0826799930a28944ba5b0582d29f9dbd
Bipul Shahi <bipulshahi25@gmail.com>
2025-03-27 10:20:35 +0530

    Update solution.sql

--- Starex_sql_project/solution.sql
+++ Starex_sql_project/solution.sql
@@ -58,12 +58,54 @@ having order_count > 10;
 select * from customers;
 
 -- Year-over-year revenue growth
+select year(sale_date) as year, sum(total_price) as revenue from sales_data
+group by  year
+order by year;
 
 -- Sales rep performance analysis
+-- number of deal closed and total revenue by each rep
+select sales_rep, count(sales_rep) as deal_closed, sum(total_price) as revenue
+from sales_data
+group by sales_rep
+order by revenue desc;
 
 -- Customers with the highest order value
+select c.customer_name, sum(s.total_price) as revenue from customers c join sales_data s
+on s.customer_id = c.customer_id
+group by c.customer_name
+order by revenue desc limit 5;
 
 -- Sales trend for a specific product category
+select p.category,date_format(s.sale_date, '%Y-%m') as month, s.total_price from sales_data s
+join products p
+on s.product_id = p.product_id;
+
+select * from products;
+
+select date_format(s.sale_date, '%Y-%m') as month, sum(s.total_price)
+as revenue from sales_data s
+join products p
+on s.product_id = p.product_id
+where p.category = "Smartphones"
+group by month
+order by month;
 
 -- Detecting potential fraud (unusually high orders)
+select customer_id, count(*) as order_count,
+sum(total_price) as total_spent from sales_data
+group by customer_id
+having total_spent > 5000000;
+
+-- orders outside 1st standard deviation
+with customer_spending as
+(
+select customer_id, sum(total_price) as total_spent from sales_data
+group by customer_id
+), 
+stats as(
+select avg(total_spent) as avg_spent,
+stddev(total_spent) as std_spent from customer_spending)
+select c.customer_id, c.total_spent from customer_spending c
+join stats s
+on c.total_spent > (s.avg_spent+s.std_spent) or c.total_spent < (s.avg_spent-s.std_spent);
 
